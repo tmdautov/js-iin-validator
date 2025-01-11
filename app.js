d20ffa18685c7906ab6edff5c5c3594da3195a2a
tmdautov <tmdautov@gmail.com>
2025-01-11 19:03:22 +0500

    changes

--- app.js
+++ app.js
@@ -97,10 +97,4 @@ function validateIIN(iin) {
     };
 }
 
-// Примеры использования:
-console.log(validateIIN('941205300125')); // Валидный ИИН
-console.log(validateIIN('941205300129')); // Неверная контрольная сумма
-console.log(validateIIN('9412053001')); // Неверная длина
-console.log(validateIIN('94120530012a')); // Содержит не только цифры
-
 module.exports = { validateIIN };
